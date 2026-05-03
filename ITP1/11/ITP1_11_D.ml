a124a5cdd48d9f061c077821d6d56ea5a090561c
yutatakabatake <yutatakabatake@gmail.com>
2026-05-03 19:16:34 +0900

    Add ITP1_11_D

---
+++ ITP1/11/ITP1_11_D.ml
@@ -0,0 +1,143 @@
+type dice = {
+  face1 : int;
+  face2 : int;
+  face3 : int;
+  face4 : int;
+  face5 : int;
+  face6 : int;
+}
+
+let init_dice nums =
+  let numbers = nums |> String.split_on_char ' ' |> List.map int_of_string in
+  {
+    face1 = List.nth numbers 0;
+    face2 = List.nth numbers 1;
+    face3 = List.nth numbers 2;
+    face4 = List.nth numbers 3;
+    face5 = List.nth numbers 4;
+    face6 = List.nth numbers 5;
+  }
+
+let pp_dice { face1; face2; face3; face4; face5; face6 } =
+  print_endline
+    ("face1= " ^ string_of_int face1 ^ "\nface2= " ^ string_of_int face2
+   ^ "\nface3= " ^ string_of_int face3 ^ "\nface4= " ^ string_of_int face4
+   ^ "\nface5= " ^ string_of_int face5 ^ "\nface6= " ^ string_of_int face6)
+
+let roll dice direction =
+  match direction with
+  | 'N' ->
+      {
+        face1 = dice.face2;
+        face2 = dice.face6;
+        face3 = dice.face3;
+        face4 = dice.face4;
+        face5 = dice.face1;
+        face6 = dice.face5;
+      }
+  | 'E' ->
+      {
+        face1 = dice.face4;
+        face2 = dice.face2;
+        face3 = dice.face1;
+        face4 = dice.face6;
+        face5 = dice.face5;
+        face6 = dice.face3;
+      }
+  | 'S' ->
+      {
+        face1 = dice.face5;
+        face2 = dice.face1;
+        face3 = dice.face3;
+        face4 = dice.face4;
+        face5 = dice.face6;
+        face6 = dice.face2;
+      }
+  | 'W' ->
+      {
+        face1 = dice.face3;
+        face2 = dice.face2;
+        face3 = dice.face6;
+        face4 = dice.face1;
+        face5 = dice.face5;
+        face6 = dice.face4;
+      }
+  | _ -> dice
+
+let rec turn_right dice =
+  {
+    face1 = dice.face1;
+    face2 = dice.face3;
+    face3 = dice.face5;
+    face4 = dice.face2;
+    face5 = dice.face4;
+    face6 = dice.face6;
+  }
+
+let eq dice1 dice2 =
+  dice1.face1 == dice2.face1 && dice1.face2 == dice2.face2
+  && dice1.face3 == dice2.face3 && dice1.face4 == dice2.face4
+  && dice1.face5 == dice2.face5 && dice1.face6 == dice2.face6
+
+let rec judge_side dice1 dice2 n =
+  if n == 0 then false
+  else if eq dice1 dice2 then true
+  else
+    let new_dice = turn_right dice1 in
+    judge_side new_dice dice2 (n - 1)
+
+let rec judge_top dice1 dice2 n =
+  match n with
+  | 1 ->
+      let new_dice = roll dice1 'E' in
+      let res = judge_side new_dice dice2 4 in
+      if res then res else judge_top dice1 dice2 (n - 1)
+  | 2 ->
+      let new_dice = roll dice1 'W' in
+      let res = judge_side new_dice dice2 4 in
+      if res then res else judge_top dice1 dice2 (n - 1)
+  | 3 ->
+      let new_dice = roll (roll (roll dice1 'N') 'N') 'N' in
+      let res = judge_side new_dice dice2 4 in
+      if res then res else judge_top dice1 dice2 (n - 1)
+  | 4 ->
+      let new_dice = roll (roll dice1 'N') 'N' in
+      let res = judge_side new_dice dice2 4 in
+      if res then res else judge_top dice1 dice2 (n - 1)
+  | 5 ->
+      let new_dice = roll dice1 'N' in
+      let res = judge_side new_dice dice2 4 in
+      if res then res else judge_top dice1 dice2 (n - 1)
+  | 6 ->
+      let res = judge_side dice1 dice2 4 in
+      if res then res else judge_top dice1 dice2 (n - 1)
+  | _ -> false
+
+let judge_same_dice dice1 dice2 = judge_top dice1 dice2 6
+
+let rec has_same_dices dices n =
+  match n with
+  | 0 -> false
+  | 1 -> has_same_dices (List.tl dices) (List.length (List.tl dices))
+  | _ ->
+      let dice1 = List.hd dices in
+      let dice2 = List.nth dices (n - 1) in
+      let res = judge_same_dice dice1 dice2 in
+      if res then res else has_same_dices dices (n - 1)
+
+let rec judge_same_dices dices = has_same_dices dices (List.length dices)
+
+let rec reader n lines : string list =
+  if n == 0 then List.rev lines
+  else
+    let line = read_line () in
+    reader (n - 1) (line :: lines)
+
+let main () =
+  let n = read_int () in
+  let lines = reader n [] in
+  let dices = List.map init_dice lines in
+  let res = judge_same_dices dices in
+  if res then print_endline "No" else print_endline "Yes"
+
+let _ = main ()
